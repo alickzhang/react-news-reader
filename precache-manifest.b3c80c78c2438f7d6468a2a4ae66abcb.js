e0b652fe97940e524299f283fd28eed975fc884a
alickzhang <alick369@gmail.com>
2020-02-17 10:22:35 +1100

    Updates

---
+++ precache-manifest.b3c80c78c2438f7d6468a2a4ae66abcb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1da86079688e4cc68e596ffaf83ebc1d",
+    "url": "/react-news-reader/index.html"
+  },
+  {
+    "revision": "066d568f463a046205c3",
+    "url": "/react-news-reader/static/css/main.49f36372.chunk.css"
+  },
+  {
+    "revision": "bd70d80f5396d8216448",
+    "url": "/react-news-reader/static/js/2.69b84118.chunk.js"
+  },
+  {
+    "revision": "98061d712500f07672e3581cfdcd5a24",
+    "url": "/react-news-reader/static/js/2.69b84118.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "066d568f463a046205c3",
+    "url": "/react-news-reader/static/js/main.508c7d25.chunk.js"
+  },
+  {
+    "revision": "b5a937c69d1298a69414",
+    "url": "/react-news-reader/static/js/runtime-main.4d0c670e.js"
+  }
+]);
